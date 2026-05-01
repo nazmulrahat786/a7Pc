57c694ef6a262e02c89ba6e63af12ee3f9497e90
Nazmul Rahat <nazmulrahat786.13@gmail.com>
2026-05-01 16:17:35 +0600

    afwea

--- src/Component/ProcesPage.jsx
+++ src/Component/ProcesPage.jsx
@@ -3,9 +3,6 @@ import ProcessCard from "./ProcessCard";
 import Complete from "./Complete";
 
 const ProcesPage = ({ inProcess, handleComplete, complete }) => {
-  
-
-  
   return (
     <div className="w-1/3 px-2 ">
       <h1 className="w-full text-2xl mb-4 font-bold ">Task Status</h1>
@@ -20,12 +17,11 @@ const ProcesPage = ({ inProcess, handleComplete, complete }) => {
         ))}
       </div>
       <h1 className="w-full text-2xl mb-4 font-bold ">Resolved Task</h1>
-      <div className=" flex flex-col gap-2">{
-
-        complete.map(cml => <Complete cml={cml} key={cml.id}></Complete>)
-        
-        
-        }</div>
+      <div className=" flex flex-col gap-2">
+        {complete.map((cml) => 
+          <Complete cml={cml} key={cml.id}></Complete>
+        )}
+      </div>
     </div>
   );
 };
